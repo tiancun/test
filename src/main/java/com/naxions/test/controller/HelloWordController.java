3a59cca4d931cfe703575ed7a07d107c877c3070
tiancun <2716804233@qq.com>
2018-06-11 10:19:01 +0800

    初始化

---
+++ src/main/java/com/naxions/test/controller/HelloWordController.java
@@ -0,0 +1,19 @@
+package com.naxions.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tiancun
+ * @date 2018-06-11
+ */
+@RestController
+public class HelloWordController {
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "HelloWord!!!!";
+    }
+
+}
